fb69379fbd1c60e63e68b54576a94eb5488fee9d
Abdu <etabdi@yahoo.com>
2019-08-06 23:19:32 -0500

    font changed

--- app.js
+++ app.js
@@ -1,62 +1,77 @@
 
-       $(document).ready(function(){ 
-                 var cars =["Ford ","Audi R8","Ferrari ","Benz","Porsche","Chevrolet "]
-       $("#add-car").on("click",function(event){
-            event.preventDefault();
-            var car =$("#car-input").val().trim();
-            cars.push(car);
-            renderButtons()
-        });
-   
-         function renderButtons(){
-         $("#buttons-view").empty();
-   for (var i=0;i<cars.length;i++){
-         var btn=$("<button>");
-         btn.addClass("car");
-         btn.attr("data-name",cars[i])
-         btn.text(cars[i]);
-         $("#buttons-view").append(btn)
-             console.log(btn)
-         }
-         }    
-        function buttonInformation(){
-            var name = $(this).attr("data-name");
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q="+name+"&api_key=3uVRFQRuRUmIUrn0Hus64PFffbVunbCw";
-            $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(queryURL)
-          var results =response.data;          
-     for (var i=0;i<cars.length;i++){
-          var carDiv=$("<div>");
-          var carImage=$("<img>");
-          carImage.addClass("carImage")
-          carImage.attr("src",results[i].images.fixed_height.url)
-          carImage.attr("data_state","still")
-          carImage.attr("src",results[i].images.fixed_height.url)
-          carDiv.append(carImage);
-            $("#view").prepend(carDiv);                 
-           }
-
-          $("#view").on('click', function() {
-            var state = $(this).attr('data-state');
-            if (state == 'still') {
-                carImage.attr("src",results[i].images.fixed_height.url)
-            } else {
-               
-                carImage.attr("src",results[i].images.fixed_height_still.url)
-             
-                console.log("click worked!");
-            }
-            });
       
-            });
-             }
-            
-           
-            renderButtons();
-            $(document).on("click",".car",buttonInformation)
+            // documen ready function
+      $(document).ready(function(){ 
+          //creating cars array
+                         var cars =["Audi R8"," Corvette","Ferrari","Benz","Porsche","Chevrolet"]
+            $("#add-car").on("click",function(event){
+                        event.preventDefault();
+                        var car =$("#car-input").val().trim();
+                        cars.push(car);
+                        renderButtons()
+                        }
+                    );
+            //add buttons for car array
+             function renderButtons(){
+                    $("#buttons-view").empty();
+                        for (var i=0;i<cars.length;i++){
+                            var btn=$("<button>");
+                            btn.addClass("car");
+                                btn.attr("data-name",cars[i])
+                                 btn.text(cars[i]);
+                                    $("#buttons-view").append(btn)
+                        console.log(btn)
+                    }
+             }   
+             //adding buttons for car name entered 
+            function buttonInformation(){
+                        var name = $(this).attr("data-name");
+                        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+name+"&api_key=3uVRFQRuRUmIUrn0Hus64PFffbVunbCw";
+                        $.ajax({
+                        url: queryURL,
+                        method: "GET"
+                    }
+                ).then(function(response) {
+                        console.log(queryURL)
+
+                    var results =response.data;          
+                        for (var i=0;i<cars.length;i++){
+                                var carDiv=$("<div>");
+                                var carImage=$("<img>");
+                                carImage.addClass("carImage")
+                                carImage.attr("src",results[i].images.fixed_height_still.url)
+                                carImage.attr("data-state","still")
+                                carImage.attr("data-still",results[i].images.fixed_height_still.url)
+                                carImage.attr("data-animate",results[i].images.fixed_height.url)
+                                carDiv.append(carImage);
+                        $("#view").prepend(carDiv);                 
+                    }
+                    
+              });
+        }                        
+                            renderButtons();
+
+
+               $(document).on("click",".car",buttonInformation)
+
+                $("#view").on('click',(".carImage") ,function() {
+                        
+                            var state = $(this).attr('data-state');
+                            if (state == 'still') {
+                            $(this).attr("src",$(this).attr("data-animate"));
+                           $(this).attr("data-state","animate");
+
+                            } 
+                            
+                    else {
+                            
+                                $(this).attr("src",$(this).attr("data-still"));
+                                $(this).att("data-state","still");
+                                                       
+                     }
+                            console.log("click worked!");
+             });
+          
             })
                 
         
