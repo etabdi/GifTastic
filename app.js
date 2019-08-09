759211b7ee191b81517febeabccbf6b0f14be386
Abdu <51891078+etabdi@users.noreply.github.com>
2019-08-09 17:19:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,16 @@
       $(document).ready(function(){ 
           //creating cars array
                          var cars =["Audi R8"," Corvette","Ferrari","Benz","Porsche","Chevrolet"]
-                         //sets up a click listener on add-car button
+                         
+                         //sets up a click listener on add-car to cars array
             $("#add-car").on("click",function(event){
                         event.preventDefault();
                         var car =$("#car-input").val().trim();
                         cars.push(car);
                         renderButtons()
-                        }
+                        };
                     );
-            //add buttons for car array
+            //add cars buttons
              function renderButtons(){
                     $("#buttons-view").empty();
                         for (var i=0;i<cars.length;i++){
@@ -24,7 +25,7 @@
                         console.log(btn)
                     }
              }   
-             //adding buttons for car name entered 
+            
             function buttonInformation(){
                         var name = $(this).attr("data-name");
                         var queryURL = "https://api.giphy.com/v1/gifs/search?q="+name+"&api_key=3uVRFQRuRUmIUrn0Hus64PFffbVunbCw";
@@ -33,8 +34,7 @@
                         method: "GET"
                     }
                 ).then(function(response) {
-                        console.log(queryURL)
-
+                       
                     var results =response.data;          
                         for (var i=0;i<cars.length;i++){
                                 var carDiv=$("<div>");
@@ -46,12 +46,11 @@
                                 carImage.attr("data-animate",results[i].images.fixed_height.url)
                                 carDiv.append(carImage);
                         $("#view").prepend(carDiv);                 
-                    }
+                    };
                     
               });
-        }                        
+        };                        
                             renderButtons();
-
 
                $(document).on("click",".car",buttonInformation)
 
